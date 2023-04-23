20e768135b54c63bfa4a04514b7befea5b4e7d73
victornavareno <victorandresnm@gmail.com>
2023-04-23 18:49:48 +0200

    Added Executors implementation for launching threads

--- src/delivery/Config.java
+++ src/delivery/Config.java
@@ -6,5 +6,10 @@ public class Config {
 	public final static int numeroMoteros = 2;			// numero de moteros por restaurante a crear
 	public final static int numeroProductos = 3; 		// limite de cantidad de productos a crear en pedido
 	public final static int maximoIdProducto = 5; 		// numero maximo de id de producto
-	public final static int maximoPrecioProducto = 5;	// precio maximo de cada producto. 
+	public final static int maximoPrecioProducto = 5;	// precio maximo de cada producto.
+	
+	// Anadidos por mi:
+	public final static int lanzarPedidos = 1;			// 0: Lanzo los threads normalmente
+														// 1: Lanzo los threads con executor
+														// 2: Lanzo los threads con streams
 }
